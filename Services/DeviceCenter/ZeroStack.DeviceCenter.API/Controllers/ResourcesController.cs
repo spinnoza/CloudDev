dc5f677083cbb99b7dce24388c3d36e8c9bc4ea9
spinoza <1176277624@qq.com>
2022-06-25 20:50:23 +0800

    添加多语言

---
+++ Services/DeviceCenter/ZeroStack.DeviceCenter.API/Controllers/ResourcesController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Localization;
+using System.Reflection;
+
+namespace ZeroStack.DeviceCenter.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ResourcesController : ControllerBase
+    {
+        private IStringLocalizerFactory _localizerFactory;
+
+        public ResourcesController(IStringLocalizerFactory localizerFactory)
+        {
+            _localizerFactory = localizerFactory;
+        }
+
+        [HttpGet]
+        public string GetHelloWorld()
+        {
+            IStringLocalizer stringLocalizer = _localizerFactory.Create("Welcome", Assembly.GetExecutingAssembly().ToString());
+
+            return stringLocalizer["HelloWorld"].Value;
+        }
+    }
+}
--- Services/DeviceCenter/ZeroStack.DeviceCenter.API/Startup.cs
+++ Services/DeviceCenter/ZeroStack.DeviceCenter.API/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
 using System;
+using System.Linq;
 using ZeroStack.DeviceCenter.API.Extensions.Tenants;
 using ZeroStack.DeviceCenter.Application;
 using ZeroStack.DeviceCenter.Domain;
@@ -26,13 +27,17 @@ namespace ZeroStack.DeviceCenter.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddLocalization(options => options.ResourcesPath = "Resources");
+
             services.AddDomainLayer().AddInfrastructureLayer(Configuration).AddApplicationLayer(); 
 
             services.AddTenantMiddleware();
 
             services.AddControllers()
                 //添加FluentValidation模型验证
-                .AddFluentValidation(fv => fv.RegisterValidatorsFromAssemblies(AppDomain.CurrentDomain.GetAssemblies())); 
+                .AddFluentValidation(fv => fv.RegisterValidatorsFromAssemblies(AppDomain.CurrentDomain.GetAssemblies()))
+                //添加模型验证本地化
+                .AddDataAnnotationsLocalization();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "ZeroStack.DeviceCenter.API", Version = "v1" });
@@ -42,6 +47,17 @@ namespace ZeroStack.DeviceCenter.API
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            #region 本地化支持
+            string[] supportedCultures = new[] { "zh-CN", "en-US" };
+            RequestLocalizationOptions localizationOptions = new RequestLocalizationOptions
+            {
+                ApplyCurrentCultureToResponseHeaders = false
+            };
+            localizationOptions.SetDefaultCulture(supportedCultures.First()).AddSupportedCultures(supportedCultures).AddSupportedUICultures(supportedCultures);
+            app.UseRequestLocalization(localizationOptions);
+            #endregion
+
+
             app.UseTenantMiddleware();
 
             //用IDataSeedProvider 初始化数据
