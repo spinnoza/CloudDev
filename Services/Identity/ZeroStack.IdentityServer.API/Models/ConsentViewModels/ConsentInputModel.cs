28581f9a61a50e10c835ac38048bb10ec25d1aa7
spinoza <1176277624@qq.com>
2022-07-06 21:41:12 +0800

    授权确认页面

---
+++ Services/Identity/ZeroStack.IdentityServer.API/Models/ConsentViewModels/ConsentInputModel.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace ZeroStack.IdentityServer.API.Models.ConsentViewModels
+{
+    public class ConsentInputModel
+    {
+        public string Button { get; set; } = null!;
+
+        public IEnumerable<string> ScopesConsented { get; set; } = null!;
+
+        public bool RememberConsent { get; set; }
+
+        public string ReturnUrl { get; set; } = null!;
+
+        public string? Description { get; set; }
+    }
+}
---
+++ Services/Identity/ZeroStack.IdentityServer.API/Models/ConsentViewModels/ProcessConsentResult.cs
@@ -0,0 +1,21 @@
+﻿using IdentityServer4.Models;
+
+namespace ZeroStack.IdentityServer.API.Models.ConsentViewModels
+{
+    public class ProcessConsentResult
+    {
+        public bool IsRedirect => RedirectUri != null;
+
+        public string RedirectUri { get; set; } = null!;
+
+        public Client? Client { get; set; }
+
+        public bool ShowView => ViewModel != null;
+
+        public ConsentViewModel? ViewModel { get; set; }
+
+        public bool HasValidationError => ValidationError != null;
+
+        public string ValidationError { get; set; } = null!;
+    }
+}
